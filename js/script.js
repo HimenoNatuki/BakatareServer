47b7dc39113915f613d9537f499ad5dab1014601
isikawayui <hakureireim8@gmail.com>
2023-06-26 21:20:23 +0900

    見出しとその他を変更

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 function kusa() {
     document.location.href = "templete/index.html";
 }
+const bgm1 = document.querySelector("#bgm1");       // <audio>
+const btn1 = document.querySelector("#btn-play1");   // <button>
+
+btn1.addEventListener("click", () => {
+    // pausedがtrue=>停止, false=>再生中
+    if (!bgm1.paused) {
+        btn1.innerHTML = '<span class="material-symbols-outlined">入店</span>';  // 「再生ボタン」に切り替え
+        bgm1.pause();
+    }
+    else {
+        btn1.innerHTML = '<span class="material-symbols-outlined">一時停止</span>';  // 「一時停止ボタン」に切り替え
+        bgm1.play();
+    }
+});
