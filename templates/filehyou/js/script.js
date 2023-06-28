78838e4dc07a6c4d0f810ccbed59f71443eed090
isikawayui <hakureireim8@gmail.com>
2023-06-28 15:08:23 +0900

    隠し要素を追加

--- templates/filehyou/js/script.js
+++ templates/filehyou/js/script.js
@@ -1,3 +1,14 @@
 function homeback() {
     location.href = "/";
 }
+
+setInterval(function () {
+    document.getElementById("d2").innerHTML = new Date().toLocaleString();
+}, 1000);
+
+function sirakamihubukichan(){
+    document.location.href= "kusa2/index.html"
+}
+
+let sirakamihubuki = document.getElementById('scretimg');
+sirakamihubuki.addEventListener('click',sirakamihubukichan)
