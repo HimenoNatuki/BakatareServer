a50fa7992c3f3d49c9a2294ec3e586ce35549c02
isikawayui <hakureireim8@gmail.com>
2023-06-28 20:27:52 +0900

    隠し要素第2段

---
+++ templates/filehyou/kusa2/js/script.js
@@ -0,0 +1,6 @@
+function sirakamihubukichan(){
+    document.location.href= "kusa3/index.html"
+}
+
+let sirakamihubuki = document.getElementById('SSRB');
+sirakamihubuki.addEventListener('click',sirakamihubukichan)
